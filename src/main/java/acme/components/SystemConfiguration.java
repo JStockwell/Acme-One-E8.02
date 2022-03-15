6844fa5361f06f6fc9c594299ab83e7b91389253
alegestor <alegestor@alum.us.es>
2022-03-15 20:12:05 +0100

    System configuration now is an entity

--- src/main/java/acme/components/SystemConfiguration.java
+++ src/main/java/acme/components/SystemConfiguration.java
@@ -1,13 +1,20 @@
 package acme.components;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 
+import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
-public class SystemConfiguration {
+public class SystemConfiguration extends AbstractEntity{
+	
+//	Serialization identifier
+	
+	protected static final long serialVersionUID = 1l;
 	
 //	Atributes
 	
