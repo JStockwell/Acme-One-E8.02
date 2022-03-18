e3bf2038e6016f9bbe575e642d163ef5a3bf09bc
Pablosancval <pablosancval@gmail.com>
2022-03-18 15:18:22 +0100

    Task IR 07: in progress
    fixed some mistakes on the code, doesnt need the complex constraints just yet, still lacks a relationship

--- src/main/java/acme/entities/patronage/Patronage.java
+++ src/main/java/acme/entities/patronage/Patronage.java
@@ -4,7 +4,11 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
@@ -13,6 +17,7 @@ import org.hibernate.validator.constraints.URL;
 
 import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,9 +26,9 @@ import lombok.Setter;
 @Setter
 public class Patronage extends AbstractEntity{
 
-	// Serialisation identif
-	protected static final long serialVersionUID = 1L;
+	// Serialisation identifier
 	
+	protected static final long serialVersionUID = 1L;
 	
 	// Attributes
 	
@@ -34,16 +39,39 @@ public class Patronage extends AbstractEntity{
 	private String code;
 	
 	@NotBlank
-	@Length(max=256) //still needs testing for the 255 256 length
+	@Length(max=255)
 	private String legislation;
 	
+	@NotNull
 	@Positive //not sure if this is enough
 	private Money budget;
 	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date creationDate;
+	
 	//TODO at least one month after its creation
-	private Date patronageDate;
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date startDate;
+	
+	//TODO at least one month after its start
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date finishDate;
 	
 	@URL
 	private String link;
 	
+	
+	// Relationships --------------------------------------------------------------------
+	
+	/*
+	@OneToMany (dont use this)
+	private Report report;
+	 */
+	
+	@ManyToOne
+	@NotNull
+	private Inventor inventor;
 }
