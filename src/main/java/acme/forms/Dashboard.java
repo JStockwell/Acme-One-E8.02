8a58f5dcbda3674f49dfbc8ebfb194a3908b5e65
alegestor <alegestor@alum.us.es>
2022-03-18 21:21:40 +0100

    Update Dashboard.java

--- src/main/java/acme/forms/Dashboard.java
+++ src/main/java/acme/forms/Dashboard.java
@@ -1,8 +1,8 @@
 package acme.forms;
 
 import java.io.Serializable;
+import java.util.Map;
 
-import acme.framework.datatypes.Money;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,28 +16,34 @@ public class Dashboard implements Serializable {
 
 	// Attributes -------------------------------------------------------------
 	
-//	The system must handle administrator dashboards with the following indicators: total number of components; average, deviation, minimum, and maximum retail price of components, grouped 
-//	by technology and currency; total number of tools; average, deviation, minimum, and maximum retail price of tools, grouped by currency; total number of pro-posed/accepted/denied patronages; average, deviation, minimum, and maximum budget of proposed/accepted/denied patronages.
+//	The system must handle administrator dashboards with the following indicators: 
+//	total number of components; average, deviation, minimum, and maximum retail price of components, grouped technology and currency; 
+//	total number of tools; average, deviation, minimum, and maximum retail price of tools, grouped by currency; 
+//	total number of pro-posed/accepted/denied patronages; average, deviation, minimum, and maximum budget of proposed/accepted/denied patronages.
 
 	Integer	totalNumberOfComponents;
-	Double averageComponentsPerTechnology;
-	Double averageComponentsPerCurrency;
-	Double deviationComponentsPerTechnology;
-	Double deviationComponentsPerCurrency;
-	Money minimumRetailPriceOfComponentsPerTechnology;
-	Money minimumRetailPriceOfComponentsPerCurrency;
-	Money maximumRetailPriceOfComponentsPerTechnology;
-	Money maximumRetailPriceOfComponentsPerCurrency;
+	Map<String, Map<String, Double>> averageComponentsPricePerTechnology;			// Map<Technology, Map<Currency, Price>>
+	Map<String, Double> averageComponentsPricePerCurrency;							// Map<Currency, Price>
+	Map<String, Map<String, Double>> deviationComponentsPricePerTechnology;			//		.
+	Map<String, Double> deviationComponentsPricePerCurrency;						//		.
+	Map<String, Map<String, Double>> minimumRetailPriceOfComponentsPerTechnology;	//		.
+	Map<String, Double> minimumRetailPriceOfComponentsPerCurrency;
+	Map<String, Map<String, Double>> maximumRetailPriceOfComponentsPerTechnology;
+	Map<String, Double> maximumRetailPriceOfComponentsPerCurrency;
 								
 	Integer	totalNumberOfTools;
-	Double averageToolsPerCurrency;
-	Double deviationToolsPerCurrency;
-	Money minimumRetailPriceOfToolsPerCurrency;
-	Money maximumRetailPriceOfToolsPerCurrency;
+	Map<String, Double> averageToolsPricePerCurrency;
+	Map<String, Double> deviationToolsPricePerCurrency;
+	Map<String, Double> minimumRetailPriceOfToolsPerCurrency;
+	Map<String, Double> maximumRetailPriceOfToolsPerCurrency;
 								
-	Integer	proposedPatronages;
-	Integer	acceptedPatronages;
-	Integer	deniedPatronages;
+//	Key -> state of patronage (proposed/accepted/denied), Value -> total of patronages with this state
+	Map<String, Integer> totalPatronages;
+	Map<String, Map<String, Double>> averagePatronageBudgetPerState;
+	Map<String, Map<String, Double>> deviationPatronageBudgetPerState;
+	Map<String, Map<String, Double>> minimumPatronageBudgetPerState;
+	Map<String, Map<String, Double>> maximumPatronageBudgetPerState;
+	
 
 	// Derived attributes -----------------------------------------------------
 
