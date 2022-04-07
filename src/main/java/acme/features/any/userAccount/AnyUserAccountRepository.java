65ee83d149c89de3bd265971492f2148498c2864
Fernando <fernandoclarosb@gmail.com>
2022-04-08 00:55:42 +0200

    Task-FR-01: In progress. Respaldo de Service Controller y Repository finalizado

---
+++ src/main/java/acme/features/any/userAccount/AnyUserAccountRepository.java
@@ -0,0 +1,22 @@
+package acme.features.any.userAccount;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.framework.entities.UserAccount;
+import acme.framework.repositories.AbstractRepository;
+
+
+
+@Repository
+public interface AnyUserAccountRepository extends AbstractRepository {
+
+	@Query("select user from UserAccount user where user.id = :id")
+	UserAccount findOneUserAccountById(int id);
+
+	@Query("select user from UserAccount user where user.enabled = true")
+	Collection<UserAccount> findEnabledUserAccounts();
+
+}
