5b9965f4f7ef9f6cb28360721c633ad122415328
Jaime Stockwell Mendoza <jstockwellmendoza@gmail.com>
2022-04-19 12:59:18 +0200

    fix: item toolkit inventor relations
    Added ManyToOne relations Item -> Inventor and Toolkit -> Inventor

--- src/main/java/acme/entities/Component.java
+++ src/main/java/acme/entities/Component.java
@@ -3,6 +3,7 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -11,6 +12,7 @@ import org.hibernate.validator.constraints.URL;
 
 import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -48,6 +50,9 @@ public class Component extends AbstractEntity{
 
 	@URL
 	protected String link;
+	
+	@ManyToOne
+	protected Inventor inventor;
 	
 
 }
--- src/main/java/acme/entities/toolkit/Toolkit.java
+++ src/main/java/acme/entities/toolkit/Toolkit.java
@@ -2,6 +2,7 @@ package acme.entities.toolkit;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -9,6 +10,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -47,5 +49,8 @@ public class Toolkit extends AbstractEntity{
 	
 	@URL
 	protected String link;
+	
+	@ManyToOne
+	protected Inventor inventor;
 
 }
