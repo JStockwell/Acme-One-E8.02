979e2c651c77b455cce058400a4f3de3a7ca5337
Gregorio <greortsol@google.com>
2022-04-22 09:48:19 +0200

    Testting and fixes
    Announcement and chirp testing donde, minor fixes from the fridays review

--- src/test/java/acme/testing/any/chirp/AnyChirpListTest.java
+++ src/test/java/acme/testing/any/chirp/AnyChirpListTest.java
@@ -11,24 +11,17 @@ public class AnyChirpListTest extends TestHarness{
 	@ParameterizedTest
 	@CsvFileSource(resources="/any/chirp/list.csv",encoding="utf-8",numLinesToSkip=1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String title, final String body, final String author, final String mail, final String moment) {
+	public void positiveTest(final int recordIndex, final String moment, final String title, final String author, final String body, final String mail) {
 		super.signIn("inventor1", "inventor1");
 
-		super.clickOnMenu("Any", "item");
+		super.clickOnMenu("Any", "Chirp");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		
 		super.checkColumnHasValue(recordIndex, 0, title);
 		super.checkColumnHasValue(recordIndex, 1, author);
 		super.checkColumnHasValue(recordIndex, 2, moment);
-
-		super.clickOnListingRecord(recordIndex);
-
-		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("author", author);
-		super.checkInputBoxHasValue("body", body);
-		super.checkInputBoxHasValue("moment", moment);
-		super.checkInputBoxHasValue("mail", mail);
+		super.checkColumnHasValue(recordIndex, 3, body);
 
 		super.signOut();
 	}
