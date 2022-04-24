e115ff694dd02940021bdf54e69f87548c562630
alegestor <alegestor@alum.us.es>
2022-04-24 19:37:34 +0200

    Fixing test

--- src/test/java/acme/testing/inventor/item/InventorItemListTest.java
+++ src/test/java/acme/testing/inventor/item/InventorItemListTest.java
@@ -9,18 +9,18 @@ import acme.testing.TestHarness;
 public class InventorItemListTest extends TestHarness{
 	
 	@ParameterizedTest
-	@CsvFileSource(resources="/inventor/item/list.csv",encoding="utf-8",numLinesToSkip=1)
+	@CsvFileSource(resources="/inventor/item/list.csv",encoding="utf-8",numLinesToSkip=1,delimiterString=";")
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String name, final String code, final String technology, final String description, final String retailPrice, final String itemtype, final String link) {
+	public void positiveTest(final int recordIndex, final String name, final String code, final String technology, final String description, final String price, final String itemtype, final String link) {
 		super.signIn("inventor1", "inventor1");
 
-		super.clickOnMenu("Any", "Item");
+		super.clickOnMenu("Inventor", "My items");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
-		
+
 		super.checkColumnHasValue(recordIndex, 0, name);
 		super.checkColumnHasValue(recordIndex, 1, technology);
-		super.checkColumnHasValue(recordIndex, 2, retailPrice);
+		super.checkColumnHasValue(recordIndex, 2, price);
 		super.checkColumnHasValue(recordIndex, 3, itemtype);
 
 		super.clickOnListingRecord(recordIndex);
@@ -29,7 +29,7 @@ public class InventorItemListTest extends TestHarness{
 		super.checkInputBoxHasValue("code", code);
 		super.checkInputBoxHasValue("technology", technology);
 		super.checkInputBoxHasValue("description", description);
-		super.checkInputBoxHasValue("price", retailPrice);
+		super.checkInputBoxHasValue("price",price);
 		super.checkInputBoxHasValue("link", link);
 		super.checkInputBoxHasValue("itemType", itemtype);
 
