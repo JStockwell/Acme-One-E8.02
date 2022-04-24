de28110eb8a3a4a859daeab48cabcff807216185
Mancbm <silverlancernohr@gmail.com>
2022-04-24 14:22:26 +0200

    Task-FR-17: Backend

--- src/main/java/acme/features/administrator/adminDashboard/AdminDashboardRepository.java
+++ src/main/java/acme/features/administrator/adminDashboard/AdminDashboardRepository.java
@@ -1,6 +1,8 @@
 package acme.features.administrator.adminDashboard;
 
-import java.util.Map;
+import java.util.List;
+
+import javax.persistence.Tuple;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,6 +15,57 @@ public interface AdminDashboardRepository extends AbstractRepository{
 	@Query("select count(i) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT")
 	int totalNumberOfComponents();
 	
-	@Query("select i.technology, i.price.currency, avg(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT group by i.price.currency, i.technology")
-	Map<String, Map<String, Double>> averageComponentsPricePerTechnology();
+	@Query("select i.technology, avg(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT and i.price.currency=:currency group by i.technology")
+	List<Tuple> averageComponentsPricePerTechnology(String currency);
+	
+	@Query("select avg(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT and i.price.currency=:currency")
+	Double averageComponentsPricePerCurrency(String currency);
+	
+	@Query("select i.technology, stddev(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT and i.price.currency=:currency group by i.technology")
+	List<Tuple> deviationComponentsPricePerTechnology(String currency);
+	
+	@Query("select stddev(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT and i.price.currency=:currency")
+	Double deviationComponentsPricePerCurrency(String currency);
+	
+	@Query("select i.technology, min(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT and i.price.currency=:currency group by i.technology")
+	List<Tuple> minimumRetailPriceOfComponentsPerTechnology(String currency);
+	
+	@Query("select min(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT and i.price.currency=:currency")
+	Double minimumRetailPriceOfComponentsPerCurrency(String currency);
+	
+	@Query("select i.technology, max(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT and i.price.currency=:currency group by i.technology")
+	List<Tuple> maximumRetailPriceOfComponentsPerTechnology(String currency);
+	
+	@Query("select max(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.COMPONENT and i.price.currency=:currency")
+	Double maximumRetailPriceOfComponentsPerCurrency(String currency);
+	
+	@Query("select count(i) from Item i where i.itemType=acme.entities.item.ItemType.TOOL")
+	int totalNumberOfTools();
+	
+	@Query("select avg(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.TOOL and i.price.currency=:currency")
+	Double averageToolsPricePerCurrency(String currency);
+	
+	@Query("select stddev(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.TOOL and i.price.currency=:currency")
+	Double deviationToolsPricePerCurrency(String currency);
+	
+	@Query("select min(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.TOOL and i.price.currency=:currency")
+	Double minimumRetailPriceOfToolsPerCurrency(String currency);
+	
+	@Query("select max(i.price.amount) from Item i where i.itemType=acme.entities.item.ItemType.TOOL and i.price.currency=:currency")
+	Double maximumRetailPriceOfToolsPerCurrency(String currency);
+	
+	@Query("select p.status, count(p) from Patronage p group by p.status")
+	List<Tuple> totalPatronages();
+	
+	@Query("select p.status, avg(p.budget.amount) from Patronage p where p.budget.currency =:currency group by p.status")
+	List<Tuple> averagePatronageBudgetPerState(String currency);
+	
+	@Query("select p.status, stddev(p.budget.amount) from Patronage p where p.budget.currency =:currency group by p.status")
+	List<Tuple> deviationPatronageBudgetPerState(String currency);
+	
+	@Query("select p.status, min(p.budget.amount) from Patronage p where p.budget.currency =:currency group by p.status")
+	List<Tuple> minimumPatronageBudgetPerState(String currency);
+	
+	@Query("select p.status, max(p.budget.amount) from Patronage p where p.budget.currency =:currency group by p.status")
+	List<Tuple> maximumPatronageBudgetPerState(String currency);
 }
