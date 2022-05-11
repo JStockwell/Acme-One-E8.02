928ea4206531712e6bfbb130d2d2be0a5cffe584
alegestor <alegestor@alum.us.es>
2022-05-11 21:04:20 +0200

    Update PatronPatronageRepository.java

--- src/main/java/acme/features/patron/patronage/PatronPatronageRepository.java
+++ src/main/java/acme/features/patron/patronage/PatronPatronageRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import acme.entities.patronage.Patronage;
 import acme.framework.repositories.AbstractRepository;
+import acme.roles.Patron;
 
 @Repository
 public interface PatronPatronageRepository extends AbstractRepository {
@@ -15,6 +16,9 @@ public interface PatronPatronageRepository extends AbstractRepository {
 	Collection<Patronage> findPatronageByPatron(int id);
 
 	@Query("SELECT pt FROM Patronage pt WHERE pt.id = :id")
-	Patronage findOne(int id);
+	Patronage findOnePatronageById(int id);
+	
+	@Query("SELECT pt FROM Patronage pt WHERE pt.patron.id = :id")
+	Patron findOnePatronById(int id);
 
 }
