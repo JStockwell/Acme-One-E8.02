a341dd061f5df05cf6a913b872715e8834c57874
GregoOS <85850373+GregoOS@users.noreply.github.com>
2022-05-13 02:12:03 -0700

    Fix

--- src/main/java/acme/features/inventor/item/InventorItemValidation.java
+++ src/main/java/acme/features/inventor/item/InventorItemValidation.java
@@ -28,15 +28,22 @@ public class InventorItemValidation {
 			existing = this.repository.findItemByCode(entity.getCode());
 			errors.state(request, existing == null, "code", "inventor.item.code.duplicated");
 		}
-			
-		final String technology = entity.getTechnology();
-		final String description = entity.getDescription();
-		final String name = entity.getName();
-		
-		errors.state(request, !this.validator.checkSpam(technology), "technology", "validator.spam");
+
+		if (!errors.hasErrors("technology")) {
+			final String technology = entity.getTechnology();
+			errors.state(request, !this.validator.checkSpam(technology), "technology", "validator.spam");
+		}
+
+		if (!errors.hasErrors("description")) {
+			final String description = entity.getDescription();
 		errors.state(request, !this.validator.checkSpam(description), "description", "validator.spam");
+		}
+
+		if (!errors.hasErrors("name")) {
+		final String name = entity.getName();
 		errors.state(request, !this.validator.checkSpam(name), "name", "validator.spam");
-		
+		}
+			
 		if (!errors.hasErrors("price")) {
 			final Double amount=entity.getPrice().getAmount();
 			final String currency=entity.getPrice().getCurrency();
