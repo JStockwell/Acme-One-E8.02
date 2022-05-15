303e2b0487ab50b13c249a4dd6fd1bf0891b9ff4
greortsol <greortsol@alum.us.es>
2022-05-15 18:19:19 +0200

    Publish and Update Item test

--- src/test/java/acme/testing/inventor/item/InventorItemPublishTest.java
+++ src/test/java/acme/testing/inventor/item/InventorItemPublishTest.java
@@ -1,6 +1,108 @@
 package acme.testing.inventor.item;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class InventorItemPublishTest {
+import acme.testing.TestHarness;
+
+public class InventorItemPublishTest extends TestHarness {
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/item/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String name, final String description, final String code, final String technology, final String price, final String link, final String itemType) {
+		super.signIn("inventor1", "inventor1");
+		
+		super.clickOnMenu("Inventor", "My items");
+
+		super.checkListingExists();
+		super.sortListing(1, "asc");
+		
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkFormExists();
+
+		super.fillInputBoxIn("name", name);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("technology", technology);
+		super.fillInputBoxIn("price", price);
+		super.fillInputBoxIn("link", link);
+		super.fillInputBoxIn("itemType", itemType);
+
+		super.clickOnSubmit("Publish");
+
+		super.clickOnMenu("Inventor", "My items");
+
+		super.checkListingExists();
+		super.sortListing(1, "asc");
+		super.checkColumnHasValue(recordIndex, 0, name);
+		super.checkColumnHasValue(recordIndex, 1, technology);
+		super.checkColumnHasValue(recordIndex, 2, price);
+		super.checkColumnHasValue(recordIndex, 3, itemType);
+
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkFormExists();
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("technology", technology);
+		super.checkInputBoxHasValue("price", price);
+		super.checkInputBoxHasValue("link", link);
+		super.checkInputBoxHasValue("itemType", itemType);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/item/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void negativeTest(final int recordIndex, final String name, final String description, final String code, final String technology, final String price, final String link, final String itemType) {
+		
+		super.signIn("inventor1", "inventor1");
+		
+		if(recordIndex==0) {
+			this.crearItem();
+		}
+		super.clickOnMenu("Inventor", "My items");
+
+		super.checkListingExists();
+		super.sortListing(1, "asc");
+		
+		super.clickOnListingRecord(0);
+
+		super.checkFormExists();
+		
+		super.fillInputBoxIn("name", name);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("technology", technology);
+		super.fillInputBoxIn("price", price);
+		super.fillInputBoxIn("link", link);
+		super.fillInputBoxIn("itemType", itemType);
+
+		super.clickOnSubmit("Publish");
+
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
+	
+	private void crearItem() {
+		super.clickOnMenu("Inventor", "Create item");
+		super.checkFormExists();
+		
+		super.fillInputBoxIn("name", "aaaaaaa");
+		super.fillInputBoxIn("description", "aaaaaaa");
+		super.fillInputBoxIn("code", "PBS-001");
+		super.fillInputBoxIn("technology", "aaaaaa");
+		super.fillInputBoxIn("price", "EUR 7");
+		super.fillInputBoxIn("link", "");
+		super.fillInputBoxIn("itemType", "COMPONENT");
+
+		super.clickOnSubmit("Create");
+	}
 
 }
