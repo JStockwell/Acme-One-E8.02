557a5b325f62db14ba1491c140eb37aa5a16a285
Pablosancval <pablosancval@gmail.com>
2022-05-30 18:54:35 +0200

    cleared useless queries

--- src/main/java/acme/features/inventor/chimpum/InventorChimpumRepository.java
+++ src/main/java/acme/features/inventor/chimpum/InventorChimpumRepository.java
@@ -8,25 +8,18 @@ import org.springframework.stereotype.Repository;
 import acme.entities.item.Chimpum;
 import acme.entities.item.Item;
 import acme.framework.repositories.AbstractRepository;
-import acme.roles.Patron;
 
 @Repository
 public interface InventorChimpumRepository extends AbstractRepository {
 
-	@Query("SELECT pt FROM Chimpum pt WHERE pt.item.inventor.id = :id")
+	@Query("SELECT pt FROM Chimpum pt WHERE pt.item.inventor.id = :id") //AND pt.item.itemType = TOOL/COMPONENT
 	Collection<Chimpum> findChimpumByInventorId(int id);
 
 	@Query("SELECT pt FROM Chimpum pt WHERE pt.id = :id")
 	Chimpum findOneChimpumById(int id);
 	
-	@Query("SELECT p FROM Patron p WHERE p.id = :id")
-	Patron findOnePatronById(int id);
-	
 	@Query("SELECT p FROM Chimpum p WHERE p.code = :code")
 	Chimpum findChimpumByCode(String code);
-	
-	@Query("SELECT ch FROM Chimpum ch")
-	Collection<Chimpum> findAllChimpums();
 	
 	@Query("SELECT i FROM Item i WHERE i.id = :id")
 	Item findOneItemById(int id);
